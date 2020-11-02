1f1557d7a8029f3992df3e5d53f1c9c060055218
Kevin Kibler <kjkibler13@gmail.com>
2020-11-01 18:54:12 -0600

    debug: corrected db host server

--- dbcon.js
+++ dbcon.js
@@ -1,7 +1,7 @@
 var mysql = require('mysql');
 var pool = mysql.createPool({
   connectionLimit : 10,
-  host            : 'mysql.eecs.oregonstate.edu',
+  host            : 'classmysql.engr.oregonstate.edu',
   user            : 'cs340_kiblerke',
   password        : '8000',
   database        : 'cs340_kiblerke'
