36969582e0577f14379825950173ff8c41328b85
Kevin Kibler <kjkibler13@gmail.com>
2020-12-06 20:26:53 -0600

    CORS stuff

--- index.js
+++ index.js
@@ -18,6 +18,13 @@ app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 app.use("/public", express.static('./public/'));
 
+// CORS
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "flip1.engr.oregonstate.edu:3000"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 // Routers
 const home = require('./routes/home.js');
 const add_entry = require('./routes/add_entry.js');
